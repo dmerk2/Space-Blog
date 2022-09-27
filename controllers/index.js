cde05322a7013b09775845276cb8b1370c86a5c7
nickdibartolo1 <107448423+nickdibartolo1@users.noreply.github.com>
2022-09-27 18:42:12 -0400

    Update index.js
    added slash to homeRoutes contant

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const apiRoutes = require("./api");
-const homeRoutes = require("..controllers./home-routes.js");
+const homeRoutes = require("../controllers./home-routes.js");
 
 router.use('/api', apiRoutes);
 router.use('/dashboard', dashboardRoutes);
