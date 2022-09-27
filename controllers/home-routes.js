cd915fa239b4a5a45af3bf18dcab12e1c1c4925d
cykj40 <102045473+cykj40@users.noreply.github.com>
2022-09-27 10:16:48 -0400

    created post routes dashboard routes and home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,14 +4,14 @@ const { Post, User, Comment } = require('../models');
 
 // get all posts for homepage
 router.get('/', (req, res) => {
-    console.log(req.session);
+    
     Post.findAll({
         attributes: [
         'id',
-        'post_url',
+        'content',
         'title',
         'created_at',
-        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        
         ],
         include: [
         {
@@ -35,6 +35,46 @@ router.get('/', (req, res) => {
             posts, 
             loggedIn: req.session.loggedIn 
         });
+        })
+        .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+        });
+    });
+
+// get one post
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+        id: req.params.id
+        },
+        attributes: [
+        'id',
+        'content',
+        'title',
+        'created_at',
+        ],
+        include: [{
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+            model: User,
+            attributes: ['username']
+            }
+        },
+        {
+            model: User,
+            attributes: ['username']
+        }
+        ]
+    })
+        .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        const post = dbPostData.get({ plain: true });
+        res.render('single-post', { post, loggedIn: req.session.loggedIn });
         })
         .catch(err => {
         console.log(err);
