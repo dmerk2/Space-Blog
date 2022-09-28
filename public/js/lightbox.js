695fd21a31f1a20aa126e72bad6de9a8647b6fc8
Ferrell4 <32971621+Ferrell4@users.noreply.github.com>
2022-09-28 16:12:00 -0400

    light box update

--- public/js/lightbox.js
+++ public/js/lightbox.js
@@ -1,6 +1,7 @@
 export default class lightbox{
     static activate(){
-        document.body.insertAdjacentHTML("beforeend", `
+        // set display to none if overiding <style="display:none;">
+        document.body.insertAdjacentHTML("beforeend",  `
             <div class="lightbox" id="lightbox">
                 <div class="lightbox__inner">
                     <button type = button class="lightbox__close">
@@ -12,5 +13,37 @@ export default class lightbox{
                 </div>
             </div>
         `);
+        
+        const lightBox = document.querySelector("#lightbox");
+        const btnClose = lightBox.querySelector(".lightbox__close");
+        const content = lightBox.querySelector(".lightbox__content");
+        const closeLightbox = () =>{
+            lightBox.style.display="none";
+            content.innerHTML = "";
+        };
+        //this makes it so you if you click outside of the box, it closes
+        // lightBox.addEventListener("mousedown", e =>{
+        //     //must be here for box not to close on anyclick
+        //     if(e.target.matches("#lightbox")){
+        //         closeLightbox();
+        //     }
+        // });
+
+        btnClose.addEventListener("click", () =>{
+            closeLightbox();
+        });
     }
+    //dont touch! overides lightbox display 
+    // static show(htmlorElement){
+    //     const content = document.querySelector("#lightbox .lightbox__content");
+
+    //     document.querySelector("#lightbox").style.display = null;
+
+    //     if(typeof htmlorElement === "string"){
+    //         content.innerHTML = htmlorElement;
+    //     }else{
+    //         content.innerHTML = "";
+    //         content.appendChild(htmlorElement);
+    //     }
+    // }
 }
