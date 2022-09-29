6f182e4d1e5e0d9d2e13303446119275a2a9c5f1
Daniel Merkin <dan.merkin@gmail.com>
2022-09-29 19:29:19 -0400

    test seeding posts

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -2,9 +2,27 @@ const { Post } = require("../models");
 
 const postData = [
   {
-    title: "What's your favorite strain?",
-    content: "SOUR DIESEL!!!!",
+    title: "Think about this!",
+    content: "If you were born deaf, what language would you think in?",
     user_id: 1,
+  },
+  {
+    title: "Do you like X-Men?",
+    content:
+      "If Professor X can move objects with his mind, why can't he make his legs move?",
+    user_id: 2,
+  },
+  {
+    title: "Hmmmm?",
+    content:
+      "If two telepaths read each other's mind, whose mind are they reading?",
+    user_id: 3,
+  },
+  {
+    title: "ALIENS EXIST!",
+    content:
+      "If the universe is always expanding, does that mean that the amount of space available for that expansion is infinite?",
+    user_id: 4,
   }
 ];
 
