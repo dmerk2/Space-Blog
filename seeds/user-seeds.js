aad0eb16af15956c2f62275621983ae96a3b5a31
Daniel Merkin <dan.merkin@gmail.com>
2022-09-29 19:29:57 -0400

    adding user seeds

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -3,9 +3,34 @@ const { User } = require('../models');
 const userData = [
   {
     user_id: 1,
-    username: "hightimes@letsgethigh.com",
-    password: "password12345"
+    username: "hightimes@aol.com",
+    password: "password12345",
   },
+  {
+    user_id: 2,
+    username: "burntout420@hotmail.com",
+    password: "password12345",
+  },
+  {
+    user_id: 3,
+    username: "letssmoke@gmail.com",
+    password: "password12345",
+  },
+  {
+    user_id: 4,
+    username: "cheech@chong.com",
+    password: "password12345",
+  },
+  {
+    user_id: 5,
+    username: "bluntz@yahoo.com",
+    password: "password12345",
+  },
+  {
+    user_id: 6,
+    username: "joints@bing.com",
+    password: "password12345",
+  }
 ];
 
 const seedUsers = () => User.bulkCreate(userData);
