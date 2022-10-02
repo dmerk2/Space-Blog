093627ba40615460356dd382b823b0a6f5399c8c
Daniel Merkin <dan.merkin@gmail.com>
2022-10-02 11:28:02 -0400

    adding email column in users table

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -3,52 +3,62 @@ const { User } = require('../models');
 const userData = [
   {
     user_id: 1,
-    username: "hightimes@aol.com",
+    username: "hightimes",
+    email: "hightimes@aol.com",
     password: "password12345",
   },
   {
     user_id: 2,
-    username: "burntout420@hotmail.com",
+    username: "burntout",
+    email: "burntout420@hotmail.com",
     password: "password12345",
   },
   {
     user_id: 3,
-    username: "letssmoke@gmail.com",
+    username: "lets_smoke",
+    email: "letssmoke@gmail.com",
     password: "password12345",
   },
   {
     user_id: 4,
     username: "cheech@chong.com",
+    email: "cheech&chong",
     password: "password12345",
   },
   {
     user_id: 5,
-    username: "bluntz@yahoo.com",
+    username: "puffPuffPass",
+    email: "bluntz@yahoo.com",
     password: "password12345",
   },
-  {
-    user_id: 6,
-    username: "joints@bing.com",
-    password: "password12345",
-  },
+  // {
+  //   user_id: 6,
+  //   username: "joint_time",
+  //   email: "bluntz@yahoo.com",
+  //   password: "password12345",
+  // },
   {
     user_id: 7,
-    username: "weed@bowls.com",
+    username: "reeeeeefer",
+    email: "weed@bowls.com",
     password: "password12345",
   },
   {
     user_id: 8,
-    username: "pot@bongs.com",
+    username: "bowls&bongs",
+    email: "bowlsandbongs@gmail.com",
     password: "password12345",
   },
   {
     user_id: 9,
-    username: "maryjane@glass.com",
+    username: "maryJane",
+    email: "maryjane@glass.com",
     password: "password12345",
   },
   {
     user_id: 10,
-    username: "grass@bing.com",
+    username: "grass",
+    email: "grass@bing.com",
     password: "password12345",
   }
 ];
