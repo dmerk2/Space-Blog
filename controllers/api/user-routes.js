320c4bf90f17d27dfe57f6847f57fe96908ff405
nickdibartolo1 <107448423+nickdibartolo1@users.noreply.github.com>
2022-10-04 22:27:27 -0400

    edits in login.js

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -80,6 +80,7 @@ router.post('/login', (req, res) => {
         username: req.body.username
         }
     }).then(dbUserData => {
+        console.log(dbUserData)
         if (!dbUserData) {
         res.status(400).json({ message: 'No user with that username!' });
         return;
